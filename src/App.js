be718dbbf2af7d46cf59673444c4d25d4bfdaa5e
Jose Augusto Megres de Oliveira <augustomegres@gmail.com>
2020-09-30 03:33:49 -0300

    Criaçao do Ranking dos melhores times de CS:GO

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
+import HLTV from 'hltv';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import FastAverageColor from 'fast-average-color';
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      teamRanking: [],
+    };
+  }
+
+  getTeams() {
+    HLTV.getTeamRanking().then((res) => {
+      this.setState({ teamRanking: res });
+    });
+  }
+
+  getChangeLogo(change) {
+    if (change > 0) {
+      return 'arrow-green.svg';
+    }
+
+    if (change === 0) {
+      return 'minus.svg';
+    }
+
+    if (change < 0) {
+      return 'arrow-red.svg';
+    }
+  }
+
+  componentDidMount() {
+    this.getTeams();
+  }
+
+  render() {
+    return (
+      <div className="team-list">
+        {this.state.teamRanking.map((item, index) => {
+          const url = `https://static.hltv.org/images/team/logo/${item.team.id}`;
+
+          const fac = new FastAverageColor();
+
+          // From not loaded image (HTMLImageElement)
+          fac
+            .getColorAsync(url, { algorithm: 'dominant' })
+            .then(function (color) {
+              console.log(color);
+              // {
+              //     rgb: 'rgb(255, 0, 0)',
+              //     rgba: 'rgba(255, 0, 0, 1)',
+              //     hex: '#ff0000',
+              //     hexa: '#ff0000ff',
+              //     value: [255, 0, 0, 255],
+              //     isDark: true,
+              //     isLight: false
+              // }
+            })
+            .catch(function (e) {
+              console.error(e);
+            });
+
+          return (
+            <div className="team">
+              <div className="logo-change">
+                <img
+                  className="logo"
+                  src={`https://static.hltv.org/images/team/logo/${item.team.id}`}
+                ></img>
+
+                <img
+                  className="change"
+                  src={`/static/${this.getChangeLogo(item.change)}`}
+                ></img>
+                <h3 className="change-ratio">{item.change}</h3>
+              </div>
+              <h1>{`${item.place} - ${item.team.name}`}</h1>
+              <h2>{item.points} Points</h2>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
